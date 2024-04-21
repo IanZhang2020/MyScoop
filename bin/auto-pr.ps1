f61f3e9db1664e293851c3be38bf7a3a86d10a03
IanZhang <ianzhang2020@qq.com>
2024-04-21 18:39:42 +0800

    update scoop scripts

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ScoopInstaller/Main:master"
+    [String]$upstream = "IanZhang/MyScoop:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
